147a6bce807545fbbcbabd2749ceb2ae88f5cc44
Boris Pasko <bpasko@exiger.com>
2018-06-29 17:19:12 -0400

    Added tests for UpdateFile

--- tallylib/rscollectionutil_test.go
+++ tallylib/rscollectionutil_test.go
@@ -1,6 +1,8 @@
 package tallylib
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -15,6 +17,69 @@ func TestUpdateFile_file_does_not_exist(t *testing.T) {
 	}
 	if ret {
 		t.Log("Should not return true")
+		t.Fail()
+	}
+}
+
+func TestUpdate_existingFile(t *testing.T) {
+	var temp, err = ioutil.TempFile("", "TestUpdate_existingFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	var path = temp.Name()
+	defer os.Remove(path)
+	defer temp.Close()
+
+	temp.WriteString("hello")
+	temp.Close()
+
+	var coll = New()
+	coll.InitEmpty()
+
+	var ret bool
+	ret, err = UpdateFile(coll, path, path, false)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if !ret {
+		t.Log("Failed to update file " + path)
+		t.Fail()
+	}
+	if coll.ByPath(path) == nil {
+		t.Log("Collection was not updated for file " + path)
+		t.Fail()
+	}
+
+	ret, err = UpdateFile(coll, path, path, false)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if ret {
+		t.Log("Collection WAS updated while it should not be")
+		t.Fail()
+	}
+
+	ret, err = UpdateFile(coll, path, path, true)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if !ret {
+		t.Log("Collection was not updated even with force=true")
+		t.Fail()
+	}
+
+	ioutil.WriteFile(path, []byte("Hello, again!"), os.ModePerm)
+
+	ret, err = UpdateFile(coll, path, path, false)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if !ret {
+		t.Log("Collection was not updated even when file changed")
 		t.Fail()
 	}
 }
