6dcb36e9852fec4678d9a412c87966108477878b
Boris Pasko <bpasko@exiger.com>
2018-07-03 16:38:30 -0400

    COmmented unused package

--- tallylib/tallystd.go
+++ tallylib/tallystd.go
@@ -1,7 +1,20 @@
 package tallylib
 
+import (
+//	"os"
+)
+
 // Holds settings
 type tally struct {
+	config TallyConfig
+}
+
+func (tally *tally) GetConfig() TallyConfig {
+	return tally.config
+}
+
+func (tally *tally) SetConfig(cfg TallyConfig) {
+	tally.config = cfg
 }
 
 func (tally *tally) UpdateSingleDirectory(directory string) error {
