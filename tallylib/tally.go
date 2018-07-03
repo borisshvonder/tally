c9e6270ba3b340161569effb51d1d39e4f41b86a
Boris Pasko <bpasko@exiger.com>
2018-07-03 18:21:12 -0400

    Wrote some untested code

--- tallylib/tally.go
+++ tallylib/tally.go
@@ -1,15 +1,45 @@
 package tallylib
 
+import (
+	"io"
+)
+
 type TallyConfig struct {
 	forceUpdate bool
+
+	// By default, tally stops on errors but continues on warnings.
+	// Set this to true and tally will act like coward.
+	stopOnWarnings bool
+
+	// Log verbosity.
+	//   0 means do not log anything,
+	//   1 - only errors,
+	//   2 - warnings + errors
+	//   3 - warnings + errors + info (default)
+	//   4 - warnings + errors + info + debug
+	// more - increase logging more and more
+	logVerbosity int
 }
 
 type Tally interface {
 	GetConfig() TallyConfig
 	SetConfig(cfg TallyConfig)
 
+	// Where to log stuff, by default don't write anywhere
+	SetLog(log io.Writer)
+
 	UpdateSingleDirectory(directory string) error
 
 	// Update all subdirectories and all parent directories (if any)
 	UpdateRecursive(directory string) error
+}
+
+type CollectionFileAccessError struct {
+	filepath string // path to file
+	message  string // additional message
+	cause    error  // underlying error, if any
+}
+
+func (e *CollectionFileAccessError) Error() string {
+	return e.filepath + " " + e.message + " " + e.cause.Error()
 }
--- tallylib/tallystd.go
+++ tallylib/tallystd.go
@@ -1,12 +1,28 @@
 package tallylib
 
 import (
-//	"os"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 // Holds settings
 type tally struct {
-	config TallyConfig
+	config      TallyConfig
+	log         io.Writer
+	loggerDebug *log.Logger
+	loggerInfo  *log.Logger
+	loggerErr   *log.Logger
+	loggerWarn  *log.Logger
+}
+
+func NewTally() Tally {
+	var ret = new(tally)
+	ret.config.logVerbosity = 3
+	ret.SetLog(ioutil.Discard)
+	return ret
 }
 
 func (tally *tally) GetConfig() TallyConfig {
@@ -17,10 +33,138 @@ func (tally *tally) SetConfig(cfg TallyConfig) {
 	tally.config = cfg
 }
 
+func (tally *tally) SetLog(logfile io.Writer) {
+	tally.log = logfile
+	tally.initLog()
+}
+
 func (tally *tally) UpdateSingleDirectory(directory string) error {
+	var collectionFile = directory + ".rscollection"
+
+	var coll, err = tally.loadExistingCollection(collectionFile)
+	if err != nil {
+		return err
+	}
+
+	var files []os.FileInfo
+	files, err = ioutil.ReadDir(directory)
+
+	for _, file := range files {
+		if !file.IsDir() {
+			var fullpath = filepath.Join(directory, file.Name())
+
+			// TODO: move to tally
+			UpdateFile(coll, fullpath, file.Name(),
+				tally.config.forceUpdate)
+		}
+	}
+
 	return nil
+}
+
+func (tally *tally) loadExistingCollection(fromFile string) (RSCollection,
+	error) {
+
+	var stat, err = os.Stat(fromFile)
+
+	if err != nil && os.IsNotExist(err) {
+
+		return nil, tally.collectionFileError(fromFile,
+			"Cannot stat", err)
+
+	} else if err == nil {
+		if stat.IsDir() {
+
+			return nil, tally.collectionFileError(fromFile,
+				"File is DIRECTORY and cannot be written to",
+				nil)
+
+		}
+	}
+
+	var file *os.File
+	file, err = os.Open(fromFile)
+	if err != nil {
+
+		return nil, tally.collectionFileError(fromFile,
+			"Cannot open", err)
+
+	}
+	var coll = NewCollection()
+	err = coll.LoadFrom(file)
+	var closeErr = file.Close()
+	if err != nil {
+		tally.warn("Cannot load file", fromFile)
+		if tally.config.stopOnWarnings {
+			tally.err("Stopping on warning")
+
+			return nil, tally.collectionFileError(fromFile,
+				"Load error", err)
+		} else {
+			tally.warn("Using empty collection (will rehash files)")
+			coll.InitEmpty()
+		}
+
+	}
+	if closeErr != nil {
+		return nil, tally.collectionFileError(fromFile,
+			"Cannot close", err)
+	}
+
+	return coll, nil
+}
+
+func (tally *tally) collectionFileError(filepath string, message string,
+	cause error) error {
+
+	var ret = new(CollectionFileAccessError)
+
+	ret.filepath = filepath
+	ret.message = message
+	ret.cause = cause
+
+	tally.err(ret)
+
+	return ret
 }
 
 func (tally *tally) UpdateRecursive(directory string) error {
 	return nil
+}
+
+func (tally *tally) initLog() {
+	tally.loggerDebug = log.New(tally.log, "DEBUG: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	tally.loggerInfo = log.New(tally.log, "INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	tally.loggerWarn = log.New(tally.log, "WARN: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	tally.loggerErr = log.New(tally.log, "ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func (tally *tally) err(v ...interface{}) {
+	if tally.config.logVerbosity >= 1 {
+		tally.loggerErr.Println(v)
+	}
+}
+
+func (tally *tally) warn(v ...interface{}) {
+	if tally.config.logVerbosity >= 2 {
+		tally.loggerWarn.Println(v)
+	}
+}
+
+func (tally *tally) info(v ...interface{}) {
+	if tally.config.logVerbosity >= 3 {
+		tally.loggerInfo.Println(v)
+	}
+}
+func (tally *tally) debug(v ...interface{}) {
+	if tally.config.logVerbosity >= 4 {
+		tally.loggerErr.Println(v)
+	}
 }
